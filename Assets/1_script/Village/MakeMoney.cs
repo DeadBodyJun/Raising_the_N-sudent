cb8bde601f93263d5ceec15e711013339766c7a5
doheeluie <chsm0115@gmail.com>
2023-07-23 11:53:42 +0900

    아르바이트쿨타임구현

--- Assets/1_script/Village/MakeMoney.cs
+++ Assets/1_script/Village/MakeMoney.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MakeMoney : MonoBehaviour
 {
@@ -9,15 +10,79 @@ public class MakeMoney : MonoBehaviour
     {
         
     }
-    public void Click()
+    
+
+    [Range(0, 20)]
+    public float MoneyTime = 10.0f;
+    [Range(0, 20)]
+    public float MoneyCoolTime = 10.0f;
+
+    public float CoolTime;
+
+    //public Text MoneyCoolTimeText;
+
+    bool isUseSkill = true;
+
+    /*private void Awake()
     {
-        GameManager.instance.Money += 2;          //게임매니저에 있는 지능값 증가
-        Debug.Log("Money");
+        skillInformationText.text = "Skill OFF";
+        MoneyCoolTimeText.text = "Skill";
+    }*/
+
+    public void Click() //아르바이트 클릭
+    {
+        if (isUseSkill == true)
+        {
+            isUseSkill = false;
+
+            //MoneyCoolTimeText.text = "";
+            CoolTime = 1;
+            GameManager.instance.Money += 10;
+            GameManager.instance.Health -= 5;
+            StartCoroutine(SkillCoroutine());
+        }
+        else
+            return;
+    }
+    IEnumerator SkillCoroutine() //스킬 사용중
+    {
+        while (CoolTime > 0)
+        {
+            CoolTime -= 1 * Time.smoothDeltaTime / MoneyTime;
+            yield return null;
+        }
+
+        StartCoroutine(ResetSkillCoroutine());
+        //StartCoroutine(CoolTimeCountCoroutine(MoneyCoolTime));
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator ResetSkillCoroutine() //스킬 쿨타임
     {
-        
+        while (CoolTime < 1)
+        {
+            CoolTime += 1 * Time.smoothDeltaTime / MoneyCoolTime;
+
+            yield return null;
+        }
+
+        isUseSkill = true;
     }
+    /*IEnumerator CoolTimeCountCoroutine(float number) //스킬 쿨타임 텍스트 표시
+    {
+        if (number > 0)
+        {
+            number -= 1;
+
+            MoneyCoolTimeText.text = number.ToString();
+
+            yield return new WaitForSeconds(1f);
+            StartCoroutine(CoolTimeCountCoroutine(number));
+        }
+        else
+        {
+            MoneyCoolTimeText.text = "Skill";
+            MoneyFillAmout.fillAmount = 0;
+            yield break;
+        }
+    }*/
 }
